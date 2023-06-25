a26a6f647028fb14bcab2177e6ca425b4553af1d
sanswed <sanswed@gmail.com>
2023-06-25 13:57:27 +0300

    step2

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,18 +1,4 @@
-# from django.views.generic import TemplateView
-from django.views.generic import View
-from pathlib import Path
-import os
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-class HomeView(View):
-
-    def get(self, request, *args, **kwargs):
-        BASE_DIR = Path(__file__).resolve().parent.parent
-        LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
-
-        return render(request, 'index.html',
-                      context={'BASE_DIR': BASE_DIR,
-                               'LOCALE_PATHS': LOCALE_PATHS,
-                              },
-                      ) 
-#    template_name = 'index.html'
+class HomeView(TemplateView):
+    template_name = 'index.html'
