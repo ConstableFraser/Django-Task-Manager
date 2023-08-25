bb7b2a7ca8ae0c6bd7a5ea14e5a528ef63427661
sanswed <sanswed@gmail.com>
2023-08-25 09:35:21 +0300

    step-final

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -102,11 +102,24 @@ AUTH_USER_MODEL = 'user.user'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'DatabaseName',
+#        'USER': 'DatabaseUserName',
+#        'PASSWORD': 'DatabaseUserpassword',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+    }
+}
+
+'''
+DATABASES = {
     'default': dj_database_url.config(
         default=DATABASE_URL,
         conn_max_age=600,
     )
 }
+'''
 
 
 # Password validation
