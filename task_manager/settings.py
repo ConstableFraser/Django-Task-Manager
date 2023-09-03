28423c94e066f7fc8049d5eb79e83836372dba06
sanswed <sanswed@gmail.com>
2023-09-03 13:42:02 +0300

    review#2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -16,10 +16,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 from django.utils.translation import gettext_lazy
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from django.urls import reverse
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
@@ -14,6 +17,10 @@ class UserLoginView(LoginView):
     template_name = 'user/user_signin.html'
 
     def get_success_url(self):
+        load_dotenv()
+        sr = os.getenv('STATIC_ROOT')
+        print("===========STATIC ROOT==========\n", sr)
+        print("===========RRR==========\n", Path(__file__).resolve().parent.parent)
         messages.success(self.request, _(USER_SIGNIN))
         return reverse('home')
 
